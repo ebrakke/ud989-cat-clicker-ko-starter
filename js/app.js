57c9988395ef4d5d75ddf50ac286071f7a671a4a
= <=>
2015-05-16 00:13:08 -0400

    Finished adding a cat list and a responsive display

--- js/app.js
+++ js/app.js
@@ -1,22 +1,47 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Fluffy');
-	this.src = ko.observable('img/22252709_010df3379e_z.jpg');
+var initialCats = [ 
+	{ name: 'Fluffy', 
+	  src: 'http://www.businessinsider.com/image/4f3433986bb3f7b67a00003c/cute-cat.jpg', 
+	  clickCount: 0,
+	  nicknames: ['F-dawg', 'Young Fluffy', 'Fluff Muff']
+	},
+    { name: 'Cloudy', 
+      src: 'http://petattack.com/wp-content/uploads/2014/07/little_cute_cat_1920x1080.jpg', 
+      clickCount: 0,
+      nicknames: ['C-breezy', 'C-Dawn', 'C-ute']
+  	},
+    { name: 'Misty', 
+      src: 'http://www.hintsandthings.co.uk/garden/cats.h1.jpg', 
+      clickCount: 0,
+  	  nicknames: ['Miester', 'Mist']
+  	}]
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.src = ko.observable(data.src);
 	this.level = ko.computed(function() {
 		if(this.clickCount() > 10) return 'Infant';
 		return 'New Born';
 	}, this);
-	this.nicknames = ko.observableArray([
-		'F-Dawg',
-		'Fluff Muff',
-		'Sir Fluffington']);
+	this.nicknames = ko.observableArray(data.nicknames);
 }
 
 var ViewModel = function() {
-	this.currentCat = ko.observable(new Cat());
-	
+	var self = this;
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catData) {
+		self.catList.push(new Cat(catData) );
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
+	}
+
+	this.changeCat = function(cat) {
+		self.currentCat(cat);
 	}
 }
 
